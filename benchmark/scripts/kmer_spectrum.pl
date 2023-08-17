cd170834aaa2b25cabf22796b12710e90e45b3a5
uensalo <uensalo@github.com>
2023-08-17 19:43:54 +0200

    Memoize paths in kmer-spectrum

--- benchmark/scripts/kmer_spectrum.pl
+++ benchmark/scripts/kmer_spectrum.pl
@@ -8,6 +8,7 @@ my @graph;  # List of lists representation
 my @labels;
 my %vertex_to_index;  # Map from vertex ID to index
 my ($input_file, $query_length);
+my %memo;  # For memoization
 
 # Parse command-line arguments
 GetOptions(
@@ -21,10 +22,10 @@ while (<$fh>) {
     chomp;
     my @fields = split /\t/;
     if ($fields[0] eq 'V') {
-        my $index = scalar(@labels);  # Current index
+        my $index = scalar(@labels);
         $vertex_to_index{$fields[1]} = $index;
         push @labels, $fields[2];
-        push @graph, [];  # Initialize an empty adjacency list for this vertex
+        push @graph, [];
     } elsif ($fields[0] eq 'E') {
         my $src_index = $vertex_to_index{$fields[1]};
         my $dst_index = $vertex_to_index{$fields[2]};
@@ -35,45 +36,31 @@ close $fh;
 
 # Function to generate a substring starting from a given vertex and offset
 sub generate_substring {
-    my ($start_vertex, $start_offset, $length) = @_;
-    my @stack;
-    push @stack, { vertex => $start_vertex, offset => $start_offset, substring => '' };
+    my ($vertex, $offset, $length, $current_string) = @_;
 
-    while (@stack) {
-        my $current = pop @stack;
-        my $vertex = $current->{vertex};
-        my $offset = $current->{offset};
-        my $substring = $current->{substring};
+    return [$current_string] if length($current_string) == $length;
+    return [] if index($labels[$vertex], 'N') != -1;
 
-        # If the substring length is reached, return it
-        if (length($substring) == $length) {
-            return $substring;
-        }
+    my $key = "$vertex:$offset:" . length($current_string);
+    return $memo{$key} if exists $memo{$key};
 
-        # If the current label contains 'N', backtrack
-        if (index($labels[$vertex], 'N') != -1) {
-            next;
-        }
+    my $remaining_label = substr($labels[$vertex], $offset);
+    my @results;
 
-        # Extract the portion of the label from the offset
-        my $remaining_label = substr($labels[$vertex], $offset);
-
-        # If the remaining label can be added entirely
-        if (length($substring) + length($remaining_label) <= $length) {
-            $substring .= $remaining_label;
-            if (defined $graph[$vertex]) {
-                # Push all outgoing vertices to the stack
-                for my $neighbor (@{$graph[$vertex]}) {
-                    push @stack, { vertex => $neighbor, offset => 0, substring => $substring };
-                }
+    if (length($current_string) + length($remaining_label) <= $length) {
+        $current_string .= $remaining_label;
+        if (defined $graph[$vertex]) {
+            for my $neighbor (@{$graph[$vertex]}) {
+                push @results, @{generate_substring($neighbor, 0, $length, $current_string)};
             }
-        } else {
-            # If only a part of the remaining label is needed
-            $substring .= substr($remaining_label, 0, $length - length($substring));
-            return $substring;
         }
+    } else {
+        $current_string .= substr($remaining_label, 0, $length - length($current_string));
+        push @results, $current_string;
     }
-    return '';
+
+    $memo{$key} = \@results;
+    return \@results;
 }
 
 # Generate all possible substrings of length q
@@ -83,8 +70,10 @@ sub generate_all_substrings {
 
     for my $vertex_index (0..$#labels) {
         for my $offset (0..length($labels[$vertex_index]) - 1) {
-            my $substring = generate_substring($vertex_index, $offset, $q);
-            $unique_strings{$substring} = 1 if length($substring) == $q;
+            my $substrings = generate_substring($vertex_index, $offset, $q, "");
+            for my $substring (@$substrings) {
+                $unique_strings{$substring} = 1 if length($substring) == $q;
+            }
         }
     }
 
