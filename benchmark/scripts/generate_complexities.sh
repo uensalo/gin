382d858f2a6161d7f41570382b008a88fff369b4
uensalo <uensalo@github.com>
2023-08-22 16:00:12 +0200

    Call gnuplot from generate_complexities.sh

--- benchmark/scripts/generate_complexities.sh
+++ benchmark/scripts/generate_complexities.sh
@@ -15,15 +15,33 @@ LOG_DIR="../log/$EXPERIMENT_NAME"
 mkdir -p "$LOG_DIR"
 BASENAME=$(basename "$INPUT_GRAPH" .fmdg)
 
+PLOT_COMMAND="set terminal pngcairo size 800,600; \
+set output '$LOG_DIR/plot_${BASENAME}_k${DEPTH}.png'; \
+set xlabel 'Path Length'; \
+set ylabel 'Frequency'; \
+plot "
+
 for ((k=1; k<=$DEPTH; k++)); do
     PATH_LOG="$LOG_DIR/paths_${BASENAME}_k$k.txt"
     ./path_generation.pl -i "$INPUT_GRAPH" -q "$k" > "$PATH_LOG"
+
     if [ "$VERTEX_COMPLEXITY" = true ]; then
         VERTEX_LOG="$LOG_DIR/vertex_complexity_${BASENAME}_k$k.txt"
         cat "$PATH_LOG" | ./vertex_complexity.pl > "$VERTEX_LOG"
     fi
+
     if [ "$EDGE_COMPLEXITY" = true ]; then
         EDGE_LOG="$LOG_DIR/edge_complexity_${BASENAME}_k$k.txt"
         cat "$PATH_LOG" | ./edge_complexity.pl > "$EDGE_LOG"
     fi
+
+    HISTOGRAM_LOG="$LOG_DIR/histogram_${BASENAME}_k$k.txt"
+    ./path_histogram.pl -i "$PATH_LOG" > "$HISTOGRAM_LOG"
+
+    # Append to the gnuplot commands
+    PLOT_COMMAND="$PLOT_COMMAND '$HISTOGRAM_LOG' using 1:2 with linespoints title 'k=$k', "
 done
+
+# Remove the last comma and space
+PLOT_COMMAND=${PLOT_COMMAND%, }
+echo "$PLOT_COMMAND" | gnuplot
