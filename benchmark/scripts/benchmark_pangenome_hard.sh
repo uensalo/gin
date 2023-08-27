044921e85a72f0c824fd706edb8275a896b12b1f
Unsal Ozturk <25726061+uensalo@users.noreply.github.com>
2023-08-27 04:55:39 +0200

    Long query benchmarks for decoding and threading

--- benchmark/scripts/benchmark_pangenome_hard.sh
+++ benchmark/scripts/benchmark_pangenome_hard.sh
@@ -68,14 +68,41 @@
 # -h
 
 # long cache benchmark
+#./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
+#-L "GRCh38-20-0.10b.fmdg_c_l2_hard" \
+#-c "0 1 2 3 4 5 6 7 8 9 10 11 12" \
+#-r "16" \
+#-m "-1" \
+#-M "-1" \
+#-j "1" \
+#-l "1024 2048 4096" \
+#-t "3600" \
+#-p "4" \
+#-h
+
+# decoding speed benchmark
 ./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
--L "GRCh38-20-0.10b.fmdg_c_l2_hard" \
--c "0 1 2 3 4 5 6 7 8 9 10 11 12" \
+-L "GRCh38-20-0.10b.fmdg_decode2_hard" \
+-c "12" \
+-r "16 32 64" \
+-m "-1" \
+-M "65536" \
+-j "1" \
+-l "16 32 64 128 256 512 1024 2048 4096" \
+-t "3600" \
+-p "4" \
+-d \
+-h
+
+# threading benchmark
+./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
+-L "GRCh38-20-0.10b.fmdg_j_l2_hard" \
+-c "12" \
 -r "16" \
 -m "-1" \
 -M "-1" \
--j "1" \
--l "1024 2048 4096" \
+-j "1 2 4 8 16 32" \
+-l "16 32 64 128 256 512 1024 2048 4096" \
 -t "3600" \
 -p "4" \
 -h
