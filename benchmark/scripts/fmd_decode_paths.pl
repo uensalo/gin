91f447b5de7f9ffa7e209ab781ada77f2f50e235
uensalo <uensalo@github.com>
2023-09-04 21:06:39 +0200

    Add usage to example path decoder script

--- benchmark/scripts/fmd_decode_paths.pl
+++ benchmark/scripts/fmd_decode_paths.pl
@@ -36,11 +36,49 @@ sub find_paths {
                 find_paths($string, $neighbor, 0, [@$path, $v], $start_o, $string_pos + $label_len);
             }
         }
-    }
-    elsif (substr($label, 0, $remaining_len) eq substr($string, $string_pos)) {
+    } elsif (substr($label, 0, $remaining_len) eq substr($string, $string_pos)) {
         my $end_o = $o + $remaining_len;
         print OUT "\t($start_o,$end_o);" . join(':', @$path, $v) . "\n";
     }
+}
+
+sub usage {
+    print << "END_USAGE";
+Description:
+    This program serves as an example path decoder. It processes the output of the 'fmd query find --decode' command. For each path root output for a given query, the script traverses the input graph file and identifies all walks that produce the corresponding string, starting from a specific offset.
+
+    Input Format (fmd query find --decode output format):
+    The input should adhere to the following semantics and syntax:
+    <string>:
+    \t(v:<integer>,o:<integer>)
+    ... (repeated for each vertex-offset pair)
+    ... (repeated for each string)
+
+    Output Format:
+    The script produces output in the following format:
+    <string>:
+    \t(o1,o2);v1:v2:...:vN
+    ... (repeated for each path)
+    ... (repeated for each string)
+
+    Semantics:
+    o1: Represents the offset into the label of the first vertex (v1) where the string starts.
+    o2: Represents the offset into the label of the last vertex (vN) where the string ends.
+
+    Example decoding pipeline call:
+    ./fmd query find -r <fmd_index> -i <query_file> -C <cache_file> --decode 2>/dev/null | fmd_decode_paths.pl -r <fmdg_file> -o <decoded_paths_file>
+
+Usage:
+    $0 -r <graph_file_name> [-i <input_file_name>] [-o <output_file_name>]
+
+Options:
+    -r <graph_file_name>    : Specify the graph file name. (Required)
+    -i <input_file_name>    : Specify the input file name. Default is stdin.
+    -o <output_file_name>   : Specify the output file name. Default is stdout.
+    --help                  : Display this help message.
+
+END_USAGE
+    exit(1);
 }
 
 my ($graph_file, $input_file, $output_file) = ('', '-', '-');
@@ -52,7 +90,17 @@ while (@ARGV) {
         $input_file = shift @ARGV || '-';
     } elsif ($arg eq '-o') {
         $output_file = shift @ARGV || '-';
+    } elsif ($arg eq '--help') {
+        usage();
+    } else {
+        print "Unknown option: $arg\n";
+        usage();
     }
+}
+
+unless ($graph_file) {
+    print "Error: -r <graph_file_name> is a required parameter.\n";
+    usage();
 }
 
 parse_graph($graph_file);
