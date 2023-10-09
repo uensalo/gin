11a26c458da4e08057b5e788b251e0a5f5f68a25
uensalo <uensalo@github.com>
2023-10-09 16:27:21 +0200

    Adjust benchmark scripts to reflect generation of logs and plots

--- benchmark/scripts/benchmark_pangenome_hard.sh
+++ benchmark/scripts/benchmark_pangenome_hard.sh
@@ -1,151 +1,46 @@
+#!/bin/bash
 #########################################################
 # Pangenomics
 #########################################################
-# query length grouped by cache depth
-# ./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
-# -L "GRCh38-20-0.10b.fmdg_c_l_hard" \
-# -c "0 2 4 6 8 10 12" \
-# -r "16" \
-# -m "-1" \
-# -M "-1" \
-# -j "1" \
-# -l "10 30 100 300 1000" \
-# -t "3600" \
-# -p "4" \
-# -h
-
-# query length grouped by matches returned
-# ./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
-# -L "GRCh38-20-0.10b.fmdg_m_l_hard" \
-# -c "10" \
-# -r "16" \
-# -m "-1 1 4 16 64 512 4096" \
-# -M "-1" \
-# -j "1" \
-# -l "10 30 100 300 1000" \
-# -t "3600" \
-# -p "4" \
-# -h
-
-# thread benchmark
-# ./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
-# -L "GRCh38-20-0.10b.fmdg_j_l_hard" \
-# -c "10" \
-# -r "16" \
-# -m "-1" \
-# -M "-1" \
-# -j "1 2 4 8 16 32" \
-# -l "10 30 100 300 1000" \
-# -t "3600" \
-# -p "4" \
-# -h
-
-# decoding speed benchmark
-# ./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
-# -L "GRCh38-20-0.10b.fmdg_decode_hard" \
-# -c "10" \
-# -r "16 32 64" \
-# -m "-1" \
-# -M "65536" \
-# -j "1" \
-# -l "10 30 100 300 500" \
-# -t "3600" \
-# -p "4" \
-# -d \
-# -h
-
-# extensive cache benchmark
-# ./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
-# -L "GRCh38-20-0.10b.fmdg_c_l2_hard" \
-# -c "0 1 2 3 4 5 6 7 8 9 10 11 12" \
-# -r "16" \
-# -m "-1" \
-# -M "-1" \
-# -j "1" \
-# -l "2 4 8 16 32 64 128 256 512" \
-# -t "3600" \
-# -p "4" \
-# -h
-
-# long cache benchmark
-#./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
-#-L "GRCh38-20-0.10b.fmdg_c_l2_hard" \
-#-c "0 1 2 3 4 5 6 7 8 9 10 11 12" \
-#-r "16" \
-#-m "-1" \
-#-M "-1" \
-#-j "1" \
-#-l "1024 2048 4096" \
-#-t "3600" \
-#-p "4" \
-#-h
-
-# decoding speed benchmark
-#./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
-#-L "GRCh38-20-0.10b.fmdg_decode2_hard" \
-#-c "12" \
-#-r "16 32 64" \
-#-m "-1" \
-#-M "65536" \
-#-j "1" \
-#-l "16 32 64 128 256 512 1024 2048 4096" \
-#-t "3600" \
-#-p "4" \
-#-d \
-#-h
-
-# threading benchmark
-#./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
-#-L "GRCh38-20-0.10b.fmdg_j_l2_hard" \
-#-c "12" \
-#-r "16" \
-#-m "-1" \
-#-M "-1" \
-#-j "1 2 4 8 16 32" \
-#-l "16 32 64 128 256 512 1024 2048 4096" \
-#-t "3600" \
-#-p "4" \
-#-h
-
 # permutation benchmark
-#./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg  \
-#-L "GRCh38-20-0.10b.fmdg_j_p_hard" \
-#-c "12" \
-#-r "64" \
-#-m "-1" \
-#-M "-1" \
-#-j "1" \
-#-l "16 32 64 128 256 512 1024 2048 4096" \
-#-t "225 450 900 1800 3600" \
-#-p "4" \
-#-h
+./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg  \
+-L "GRCh38-20-0.10b.fmdg_j_p_hard" \
+-c "12" \
+-r "32" \
+-m "-1" \
+-M "-1" \
+-j "1" \
+-l "16 32 64 128 256 512 1024 2048 4096" \
+-t "225 450 900 1800 3600" \
+-p "12" \
+-h
 
-# long cache benchmark 2
-#./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
-#-L "GRCh38-20-0.10b.fmdg_principal" \
-#-c "0 1 2 3 4 5 6 7 8 9 10 11 12" \
-#-r "16" \
-#-m "-1" \
-#-M "-1" \
-#-j "1" \
-#-l "16 32 64 128 256 512 1024 2048 4096" \
-#-t "3600" \
-#-p "4" \
-#-h
+# principal benchmark
+./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
+-L "GRCh38-20-0.10b.fmdg_principal" \
+-c "0 1 2 3 4 5 6 7 8 9 10 11 12" \
+-r "32" \
+-m "-1" \
+-M "-1" \
+-j "1" \
+-l "16 32 64 128 256 512 1024 2048 4096" \
+-t "3600" \
+-p "12" \
+-h
 
-# decoding speed benchmark
-#./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
-#-L "GRCh38-20-0.10b.fmdg_rate_decode" \
-#-c "12" \
-#-r "16 32 64" \
-#-m "-1" \
-#-M "-1" \
-#-j "1" \
-#-l "16 32 64 128 256 512 1024 2048 4096" \
-#-t "3600" \
-#-p "4" \
-#-d \
-#-h
+# decode benchmark
+./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
+-L "GRCh38-20-0.10b.fmdg_rate_decode" \
+-c "12" \
+-r "32" \
+-m "-1" \
+-M "-1" \
+-j "1" \
+-l "16 32 64 128 256 512 1024 2048 4096" \
+-t "3600" \
+-p "12" \
+-d \
+-h
 
 # plain benchmark
 ./fmd_run.sh ../input/GRCh38-20-0.10b.fmdg \
@@ -170,7 +65,7 @@
 -j "1" \
 -l "16 32 64 128 256 512 1024 2048 4096" \
 -t "3600" \
--p "4" \
+-p "12" \
 -h
 
 # cache baseline benchmark
@@ -196,5 +91,5 @@
 -j "1" \
 -l "16 32 64 128 256 512 1024 2048 4096" \
 -t "3600" \
--p "4" \
+-p "12" \
 -h
