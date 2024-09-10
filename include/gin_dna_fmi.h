4879826342152f86df968476fd0052a5e1870795
uensalo <uensalo@github.com>
2024-09-10 13:36:18 +0200

    Redesign DNA FMI and fit everything into cache lines

--- include/gin_dna_fmi.h
+++ include/gin_dna_fmi.h
@@ -11,58 +11,160 @@
  * In particular, gin_dna_fmi is optimised for an alphabet size of 8, which is the bare minimum
  * required for gin_gin over DNA ($, c0, c1, A, C, G, T, N). The permutation is implicitly stored.
  *
- * It uses a cache-optimised bitvector:
- *      Each superblock has two blocks.
- *      Each block contains counts for 256 bits.
- *      Hence, each superblock contains 64 bytes and bitvector accesses are cache aligned.
- *      Each superblock header is 64 bytes:
- *          7 chars * 8 byte counts each + 7 chars * 1 byte count each + 1 byte carry bits
- *
- * The L column is stored as flat wavelets in six bitvectors. First bitvector is set
- *
- * Buffer write order:
- * no_chars (8 bytes)
- * inverse suffix array sampling rate (8 bytes)
- * f column cumsum (64 bytes, 8 bytes per character)
- * headers (64 bytes per superblock, N/64 superblocks)
- *      header structure 7 chars x 8 byte counts + 7 chars x 1 byte count + 1 byte "full"
- * primary bitvectors (4, non-interleaved, in ceil(N/8) bytes)
- * secondary bitvectors (2, non-interleaved, in ceil(N/8) bytes)
- * sa occupancy header (1, in 8*ceil(N/8) bytes)
- * sa occupancy bitvector (1, in ceil(N/8) bytes)
- * sa values (8 bytes per entry)
- * permutation bitvector (1, in ceil(M/8) bytes)
  */
 
 #define DFMI_SET_BIT(ARRAY, K) ((ARRAY)[(K) >> 6] |= (UINT64_C(1) << ((K) & 63)))
 #define DFMI_GET_BIT(ARRAY, K) (((ARRAY)[(K) >> 6] >> ((K) & 63)) & 1)
 #define DFMI_POPCOUNT(X) __builtin_popcountll(X)
 
-typedef union gin_dfmi_header_ {
+#define DFMI_NO_CHARS 6
+
+#define DFMI_A 0
+#define DFMI_C 1
+#define DFMI_G 2
+#define DFMI_T 3
+#define DFMI_N 4
+#define DFMI_X 5
+
+const static char gin_dfmi_dec[DFMI_NO_CHARS] = {
+        'A', 'C', 'G', 'T', 'N', '('
+};
+
+const static int8_t gin_dfmi_enc[256] = {
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1,  DFMI_X, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1,  DFMI_A, -1,  DFMI_C, -1, -1, -1,  DFMI_G, -1, -1, -1, -1, -1, -1,  DFMI_N, -1,
+        -1, -1, -1, -1,  DFMI_T, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+};
+
+inline static uint64_t gin_dfmi_wavelet_enc(uint8_t enc, const uint64_t *bv) {
+    switch (enc) {
+        case DFMI_A:  // A: 001
+            return ((!bv[0]) & (!bv[1]) &   bv[2]);
+        case DFMI_C:  // C: 010
+            return ((!bv[0]) &   bv[1]  & (!bv[2]));
+        case DFMI_G:  // G: 011
+            return ((!bv[0]) &   bv[1]  &   bv[2]);
+        case DFMI_T:  // T: 100
+            return (bv[0]    & (!bv[1]) & (!bv[2]));
+        case DFMI_N:  // N: 101
+            return (bv[0]    & (!bv[1]) &   bv[2]);
+        case DFMI_X:  // X: 110
+            return ((!bv[0]) & (!bv[1]) &   bv[2]);
+        default:
+            fprintf(stderr, "[gin_dna_fmi]: invalid encoding\n");
+            return -1;
+    }
+}
+
+/*
+ * 40 bit unsigned integer for popcounts
+ */
+typedef struct gin_uint40_ {
+    uint8_t bytes[5];
+} gin_uint40_t;
+
+inline static uint64_t gin_uint40_get(gin_uint40_t u) {
+    uint64_t result = 0;
+    memcpy(&result, u.bytes, sizeof(u.bytes));
+    return result;
+}
+
+inline static void gin_uint40_set(gin_uint40_t *u, uint64_t val) {
+    memcpy(u->bytes, &val, sizeof(u->bytes));
+}
+
+/*
+ * Cache aligned superblock - fits in two cache lines (2x64).
+ * Can prefetch the second line before a memory access is made if necessary
+ * Stores counts of 6 characters: A, C, G, T, N, c0. Other chars are not used
+ * for rank queries. The ranks are computed as follows: Each char is assigned
+ * a codeword, which indicates how the char is to be recovered from the bvs:
+ *  - A: 001: !bv[0] &  !bv[1] &  bv[2]
+ *  - C: 010: !bv[0] &   bv[1] & !bv[2]
+ *  - G: 011: !bv[0] &   bv[1] &  bv[2]
+ *  - T: 100:  bv[0] &  !bv[1] & !bv[2]
+ *  - N: 101:  bv[0] &  !bv[1] &  bv[2]
+ *  - X: 110: !bv[0] &  !bv[1] &  bv[2]
+ */
+typedef union gin_dfmi_sbc_ {
+    uint64_t buf[16]; // 128 bytes per superblock
+    struct {
+        gin_uint40_t sbc[DFMI_NO_CHARS]; // 6x5 byte integers, can index up to 1 TB
+        uint8_t pad[2];                // NOT used, here for cache alignment
+        struct {
+            uint8_t  bc[DFMI_NO_CHARS];  // cumulative sum of each char in the block
+            uint8_t  pad[2]; // NOT used, here for cache alignment
+            uint64_t bv[3];  // bitvectors for rank access
+        } blocks[3];
+    } sbc;
+} gin_dfmi_sbc_t;
+
+/*
+ * Cache aligned suffix array occupancy bitvector - fits in one cache line.
+ * popcnt stores the number of set bits up until the beginning of the block.
+ */
+typedef union gin_dfmi_sao_ {
     uint64_t buf[8];
     struct {
-        uint64_t sblock_ct[7];
-        uint8_t block_ct[7];
-        uint8_t carry;
-    } cache;
-} gin_dfmi_header_t;
+        gin_uint40_t popcnt; // popcount until this point
+        uint16_t bc[5];      // cumulative in-block popcount
+        uint64_t bv[6];      // actual bitvectors
+    } sa_occ_sbc;
+} gin_dfmi_sao_t;
 
+/*
+ * The L column is stored as flat wavelets in three bitvectors.
+ *
+ * Buffer write order:
+ * no_chars (8 bytes)
+ * inverse suffix array sampling rate (8 bytes)
+ * Position of original string terminator in the BWT (8 bytes)
+ * 40 byte padding for cache alignment, may be used for future use
+ * F column cumsum (64 bytes, 8 bytes per character)
+ * L column (2 cache lines per superblock):
+ *      128-byte superblocks, ceil(N/192) many:
+ *          32 byte sblock header:
+ *              6x5 byte cumulative counts
+ *              2 byte padding
+ *          3 x 32 byte blocks:
+ *              6 byte cumulative counts
+ *              2 byte padding
+ *              24 byte bitvectors (bv0 bv1 bv2)
+ * SA occupancy (1 cache line per superblock):
+ *      64-byte superblocks, ceil(N/384) many:
+ *          5 byte popcount
+ *          4x2bytes intermediate cumulative sum
+ *          6 blocks:
+ *              8 byte payload
+ * SA values (5 bytes per entry, ceil(N/isa_r) many)
+ * permutation bitvector (1, in ceil(M/8) bytes)
+ */
 typedef struct gin_dfmi_ {
     // stored fields
-    uint64_t size;      // size in bytes of the dfmi
-    uint64_t *buf;      // buffer containing *everything*
+    uint64_t size;          // size in bytes of the dfmi
+    uint64_t *buf;          // buffer containing *everything*
     // non-stored fields: these are in-memory pointers for convenience, derived from the buffer
-    uint64_t no_chars;  // includes the terminator and the permutation
-    uint64_t isa_r;     // suffix array sampling rate
-    uint64_t *f;        // 8 element f column cumsum:   $, c0, c1, A, C, G, N, T
-                        // 0,1 are skipped over - still reflected in A
-    gin_dfmi_header_t *headers;  // superblocks & blocks
-    uint64_t *bv_1[4];  // primary bitmaps:   00x,01x,10x,11x
-    uint64_t *bv_2[2];  // secondary bitmaps: xx0,xx1
-    uint64_t *bv_sa_h;  // sa headers: popcount cache
-    uint64_t *bv_sa_occ;// sa bitmap:  bit set if isa sampled
-    uint64_t *sa;       // sa entries themselves
-    uint64_t *bv_p;     // perm bitmap: stores permutation entries, needed for decoding
+    uint64_t no_chars;      // includes the terminator and the permutation
+    uint64_t isa_r;         // suffix array sampling rate
+    uint64_t str_term_pos;  // string terminator position in L
+    uint64_t *f;            // 8 element f column cumsum:   $, c0, c1, A, C, G, T, N
+    gin_dfmi_sbc_t *lf;     // superblocks & blocks
+    gin_dfmi_sao_t *sa_occ; // sa headers: popcount cache
+    uint64_t *sa;           // sa entries themselves
+    uint64_t *bv_p;         // perm bitmap: stores permutation entries, needed for decoding
 } gin_dfmi_t;
 
 gin_dfmi_t* gin_dfmi_build(const char* str, uint64_t size, uint64_t isa_rate);
--- src/gin_dna_fmi.c
+++ src/gin_dna_fmi.c
@@ -4,61 +4,8 @@
 gin_dfmi_t* gin_dfmi_build(const char* str, uint64_t size, uint64_t isa_rate) {
     gin_dfmi_t *dfmi = calloc(1, sizeof(gin_dfmi_t));
     if(!dfmi) return NULL;
-    uint64_t nb = ((size-1) >> 8) + 1;
-    uint64_t nsb = ((nb-1) >> 1) + 1;
-    uint64_t nbv_words = ((size-1) >> 6) + 1;
-    uint64_t n_isa = (size-1) / isa_rate + 1;
-    uint64_t n_sa_occ_bv_blocks = ((size-1) >> 9) + 1;
-    uint64_t n_sa_occ_bv_words = nbv_words;
-    uint64_t n_total_words = 10 +                   // no_chars, isa_r, F
-                             8 * nsb +              // headers, L
-                             6 * nbv_words +        // bitvectors, L
-                             n_sa_occ_bv_blocks +   // popcount for sa occupancy (1 word per block)
-                             n_sa_occ_bv_words +    // sa occupancy bitvector
-                             n_isa +                // sa entries
-                             nbv_words;             // permutation
-    uint64_t *buf = calloc(n_total_words, sizeof(uint64_t));
 
-    // populate starts and write simple stuff to the bs
-    dfmi->buf = buf;
-    buf[0] = size;
-    buf[1] = isa_rate;
-    dfmi->no_chars = size;
-    dfmi->isa_r = isa_rate;
-
-    //pointers to places
-    dfmi->f = &buf[2];
-    dfmi->headers = (gin_dfmi_header_t*)(&buf[10]);
-    for(uint64_t i = 0; i < 4; i++) {
-        dfmi->bv_1[i] = &buf[10 + (8 * nsb) + i * nbv_words];
-    }
-    for(uint64_t i = 0; i < 2; i++) {
-        dfmi->bv_2[i] = &buf[10 + (8 * nsb) + (4 + i) * nbv_words];
-    }
-    dfmi->bv_sa_h   = &buf[10 + (8 * nsb) + (6 * nbv_words)];
-    dfmi->bv_sa_occ = &buf[10 + (8 * nsb) + (6 * nbv_words) + n_sa_occ_bv_blocks];
-    dfmi->sa   = &buf[10 + (8 * nsb) + (6 * nbv_words) + n_sa_occ_bv_blocks + n_sa_occ_bv_words];
-    dfmi->bv_p = &buf[10 + (8 * nsb) + (7 * nbv_words) + n_sa_occ_bv_blocks + n_sa_occ_bv_words];
-    // Compute the SA
-    int64_t *sa = calloc(size+1, sizeof(uint64_t));
-    divsufsort64((sauchar_t*)str, (saidx64_t*)sa, (saidx64_t)size+1);
-
-    char *bwt = (char*)malloc((size+1)*sizeof(char));
-    // Compute the BWT
-    uint64_t k = 0;
-    for(uint64_t i = 0; i < size+1; i++) {
-        bwt[i] = sa[i] ? str[sa[i] - 1] : (char)'\0';
-        if(!(sa[i] % isa_rate)) {
-            DFMI_SET_BIT(dfmi->bv_sa_occ, i);
-            dfmi->sa[k++] = sa[i];
-        }
-    }
-    // SA is no longer needed, free here
-    free(sa);
-    // Populate bitvectors
-    for(uint64_t i = 0; i < size; i++) {
-
-    }
+    return dfmi;
 }
 
 
