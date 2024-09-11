af86fcda369701a65e5113d567eda9c8a5956294
uensalo <uensalo@github.com>
2024-09-11 12:59:42 +0200

    Update bootstrap.sh to handle fast bitvector benchmarks

--- benchmark/scripts/bootstrap.sh
+++ benchmark/scripts/bootstrap.sh
@@ -3,8 +3,12 @@ mkdir -p ../bin
 mkdir -p ../log
 mkdir -p ../res
 mkdir -p ../res/index
+mkdir -p ../res/index-sdsl
+mkdir -p ../res/index-fast
 mkdir -p ../res/query
 mkdir -p ../res/permutation
 mkdir -p ../res/cache
+mkdir -p ../res/cache-sdsl
+mkdir -p ../res/cache-fast
 mkdir -p ../input
 mkdir -p ../plot
